2c49d08b3eab8c3150c2fb45846252ad3de01a9d
HoeZhiWan <hoezhiwan@graduate.utm.my>
2024-12-08 14:12:08 +0800

    added menu

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 // *******************************************************************
 
 void function1() { // change the name and datta type of function
-
+    cout << "Function 1" << endl;
 }
 
 // *******************************************************************
@@ -19,7 +19,7 @@ void function1() { // change the name and datta type of function
 // *******************************************************************
 
 void function2() { // change the name and datta type of function
-
+    cout << "Function 2" << endl;
 }
 
 // *******************************************************************
@@ -29,7 +29,7 @@ void function2() { // change the name and datta type of function
 // *******************************************************************
 
 void function3() { // change the name and datta type of function
-
+    cout << "Function 3" << endl;
 }
 
 // *******************************************************************
@@ -39,7 +39,7 @@ void function3() { // change the name and datta type of function
 // *******************************************************************
 
 void function4() { // change the name and datta type of function
-
+    cout << "Function 4" << endl;
 }
 
 // *******************************************************************
@@ -48,19 +48,45 @@ void function4() { // change the name and datta type of function
 //                                                                   *
 // *******************************************************************
 
+void displayMenu() {
+    cout << "1. " << endl;
+    cout << "2. " << endl;
+    cout << "3. " << endl;
+    cout << "4. Exit" << endl;
+    cout << "Choose an option (1-4): ";
+}
+
 int main() {
+    int choice;
 
-    // describe function
-    function1();
+    do{
+        displayMenu();
+        cin >> choice;
+        
+        switch(choice) {
+            
+            case 1: cout << "\n***** Module 1 ******" << endl;
+                    function1();
+                    cout << "***** Module 1 ******\n" << endl;
+                    break;
 
-    // describe function
-    function2();
+            case 2: cout << "\n***** Module 2 ******" << endl;
+                    function2();
+                    function3();
+                    cout << "***** Module 2 ******\n" << endl;
+                    break;
 
-    // describe function
-    function3();
+            case 3: cout << "\n***** Module 3 ******" << endl;
+                    function4();
+                    cout << "***** Module 3 ******\n" << endl;
+                    break;
+            
+            case 4: cout << "Exiting program...";
+                    break;
 
-    // describe function
-    function4();
+            default: cout << "Invalid Option! Please enter a number between 1 and 4.\n" << endl;
+        }
+    } while(choice!=4);
 
     return 0;
 }
