0ee69c7c75d74793ca7766d9b8e6d014ed3418ca
HoeZhiWan <hoezhiwan@graduate.utm.my>
2024-12-08 13:58:03 +0800

    function flow

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+// *******************************************************************
+//          Function 1 -                                             *
+//          Student Name: Tang Yu Shan                               *
+//          Matrik Number: A24CS0199                                 *
+// *******************************************************************
+
+void function1() { // change the name and datta type of function
+
+}
+
+// *******************************************************************
+//          Function 2 -                                             *
+//          Student Name: Bong Zi Shan                               *
+//          Matrik Number: A24CS0231                                 *
+// *******************************************************************
+
+void function2() { // change the name and datta type of function
+
+}
+
+// *******************************************************************
+//          Function 3 -                                             *
+//          Student Name: Hoe Zhi Wan                                *
+//          Matrik Number: A24CS0082                                 *
+// *******************************************************************
+
+void function3() { // change the name and datta type of function
+
+}
+
+// *******************************************************************
+//          Function 4 -                                             *
+//          Student Name: Md Abdullah                                *
+//          Matrik Number: A24CS4022                                 *
+// *******************************************************************
+
+void function4() { // change the name and datta type of function
+
+}
+
+// *******************************************************************
+//                                                                   *
+//                         Main Function                             *
+//                                                                   *
+// *******************************************************************
+
+int main() {
+
+    // describe function
+    function1();
+
+    // describe function
+    function2();
+
+    // describe function
+    function3();
+
+    // describe function
+    function4();
+
+    return 0;
+}
