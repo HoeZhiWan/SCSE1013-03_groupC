a29c2da2a17b8128aa704929608560375f047aaa
HoeZhiWan <hoezhiwan@graduate.utm.my>
2024-12-11 15:48:00 +0800

    function3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // *******************************************************************
-//          Function 1 -                                             *
+//          Function 1 - Recycling Entry                             *
 //          Student Name: Tang Yu Shan                               *
 //          Matrik Number: A24CS0199                                 *
 // *******************************************************************
@@ -46,7 +46,7 @@ void wasteInput(string type[],double weight[],string date[], int &entryCount)
     }
 }
 // *******************************************************************
-//          Function 2 -                                             *
+//          Function 2 - Calculate Statistics                        *
 //          Student Name: Bong Zi Shan                               *
 //          Matrik Number: A24CS0231                                 *
 // *******************************************************************
@@ -86,9 +86,9 @@ void calculateTotals(int entryCount, string type[], double weight[], double mate
     }
 }
 
-void displayResults(int entryCount, string type[], double weight[], string date[], double materialWeights[], int materialCounts[], double totalWeight) 
+void displayResults(int entryCount, string type[], double weight[], string date[], double materialWeights[], int materialCounts[], double totalWeight, string categories[]) 
 {
-    cout<<"----------------------------------------------------------------------------------------"<<endl;
+    cout<<"\n----------------------------------------------------------------------------------------"<<endl;
     cout<<setw(8)<<"Date"<<setw(20)<<"Material"<<setw(37)<<"Weight(kg)/Volume(L)"<<endl;
     cout<<"----------------------------------------------------------------------------------------"<<endl;
 
@@ -117,7 +117,6 @@ void displayResults(int entryCount, string type[], double weight[], string date[
              << setw(25)<<fixed<<setprecision(2)<<weight[i]<<endl;
     }
 
-    string categories[]={"Paper","Glass","Metal","Plastic"};
     string categoryNames[]={"Paper (Recyclable)","Glass (Recyclable)","Metal (Recyclable)","Plastic (Recyclable)"};
     double recyclingPercentages[4];
 
@@ -150,23 +149,66 @@ void displayResults(int entryCount, string type[], double weight[], string date[
         totalMaterials += materialCounts[i];
     }
     cout<<"Total Materials Regardless of Category: "<<totalMaterials<<endl;
-    cout<<"---------------------------------------------------------------------------------------------"<<endl;
+    cout<<"---------------------------------------------------------------------------------------------\n"<<endl;
 }
 
 // *******************************************************************
-//          Function 3 -                                             *
+//          Function 3 - Environmental Impact Assessment             *
 //          Student Name: Hoe Zhi Wan                                *
 //          Matrik Number: A24CS0082                                 *
 // *******************************************************************
 
-void function3(string entryType[], double entryWeight[], string entryDate[]) { // change the name and datta type of function
-    for (int i=0; i<8; i++) {
-        cout << entryType[i] << " " << entryWeight[i] << " " << entryDate[i] << endl;
+void environmentalImpact(double materialWeights[], string categories[]) {
+    double impactFactors[] = {0.079, 3.96, 2.33, 0.17};
+
+    // sources for impactFactors: 
+    // https://www.drugplastics.com/resource-hub/blog/choose-plastic-over-glass-for-better-sustainability 
+    // https://www.quora.com/How-much-CO2-is-produced-per-KWH-of-electricity
+    // https://www.encon.eu/en/calculation-co2
+    // https://www.milgro.eu/en/blog/co2-impact-of-paper-use-three-strategies-to-reduce-it
+    // https://8billiontrees.com/carbon-offsets-credits/carbon-footprint-of-steel
+    // https://sustainability.um.edu.my/ghg-um-2023
+
+    double totalTreesSaved = 0; // plastic & paper tree per kg
+    double totalEnergySaved = 0; // glass & metal kwh per kg
+
+    cout << "\n---------------------------------------------------------------------------------------------" << endl;
+    cout << "                                  Environmental Impact Assessment                            " << endl;
+    cout << "---------------------------------------------------------------------------------------------" << endl;
+    cout << "Categories \t Total Trees/Energy Saved" << endl;
+    cout << "---------------------------------------------------------------------------------------------" << endl;
+
+    for (int i=0; i<4; i++) {
+        double impactValue = 0;
+
+        if (categories[i] == "Paper" || categories[i] == "Plastic") {
+            impactValue = materialWeights[i] * impactFactors[i];
+
+            cout << categories[i]
+                << "\t\t  " << fixed << setprecision(2) << impactValue
+                << "\t trees" << endl;
+
+            totalTreesSaved += materialWeights[i] * impactFactors[i];
+        } else if (categories[i] == "Glass" || categories[i] == "Metal") {
+            impactValue += materialWeights[i] * impactFactors[i];
+
+            cout << categories[i]
+                << "\t\t  " << fixed << setprecision(2) << impactValue 
+                << "\t kwh of energy" << endl;
+
+            totalEnergySaved += materialWeights[i] * impactFactors[i];
+        }
     }
+
+    cout << "---------------------------------------------------------------------------------------------" << endl;
+    cout << "Total trees saved\t: " << totalTreesSaved << " trees" << endl;
+    cout << "Total energy saved\t: " << totalEnergySaved << "kwh" << endl;
+    cout << "---------------------------------------------------------------------------------------------\n" << endl;
+
 }
 
 // *******************************************************************
-//          Function 4 -                                             *
+//          Function 4 - Generate Report                             *
 //          Student Name: Md Abdullah                                *
 //          Matrik Number: A24CS4022                                 *
 // *******************************************************************
@@ -183,24 +225,30 @@ void function4() { // change the name and datta type of function
 
 
 void displayMenu() {
-    cout << "1. Create an entry." << endl;
-    cout << "2. Recycling Calculation." << endl;
-    cout << "3. " << endl;
-    cout << "4. Exit" << endl;
-    cout << "Choose an option (1-4): ";
+    cout << "1. Add Recycling Entry" << endl;
+    cout << "2. View Recycling Statistics" << endl;
+    cout << "3. Enviromnetal Impact Assessment" << endl;
+    cout << "4. Generate a Report" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Choose an option (1-5): ";
 }
 
 int main() {
 
     // Dummy Data
     const int MAX_ENTRIES = 1024;
+    
     string entryType[MAX_ENTRIES] = {"Plastic", "Glass", "Glass", "Plastic", "Paper", "Paper", "Metal", "Glass"};
     double entryWeight[MAX_ENTRIES] = {0.5, 2.2, 1.6, 0.8, 0.4, 0.7, 2.3, 0.8};
     string entryDate[MAX_ENTRIES] = {"2024-12-01", "2024-12-02", "2024-12-02", "2024-12-03", "2024-12-04", "2024-12-04", "2024-12-06", "2024-12-07"};
     int entryCount = 8;
+    
     double materialWeights[4] = {0, 0, 0, 0}; 
     int materialCounts[4] = {0, 0, 0, 0};
     double totalWeight = 0;
+    
+    string categories[]={"Paper","Glass","Metal","Plastic"};
+
     int choice;
 
     do{
@@ -209,29 +257,32 @@ int main() {
         
         switch(choice) {
             
-            case 1: cout << "\n***** Module 1 ******" << endl;
+            case 1: cout << "\n***** Add an entry ******" << endl;
                     wasteInput(entryType, entryWeight, entryDate, entryCount);
-                    cout << "***** Module 1 ******\n" << endl;
+                    cout << "***** Add an entry ******\n" << endl;
                     break;
 
-            case 2: cout << "\n***** Module 2 ******" << endl;
-                    calculateTotals(entryCount, entryType, entryWeight, materialWeights, materialCounts, totalWeight);
-                    displayResults(entryCount, entryType, entryWeight, entryDate, materialWeights, materialCounts, totalWeight);
-
-                    cout << "***** Module 2 ******\n" << endl;
+            case 2: calculateTotals(entryCount, entryType, entryWeight, materialWeights, materialCounts, totalWeight);
+                    displayResults(entryCount, entryType, entryWeight, entryDate, materialWeights, materialCounts, totalWeight, categories);
                     break;
 
-            case 3: cout << "\n***** Module 3 ******" << endl;
+            case 3: calculateTotals(entryCount, entryType, entryWeight, materialWeights, materialCounts, totalWeight);
+                    environmentalImpact(materialWeights, categories);
+                    break;
+
+            case 4: cout << "\n***** Module 4 ******" << endl;
                     function4();
-                    cout << "***** Module 3 ******\n" << endl;
+                    cout << "***** Module 4 ******\n" << endl;
                     break;
             
-            case 4: cout << "Exiting program...";
+            case 5: cout << "Exiting program...";
                     break;
 
-            default: cout << "Invalid Option! Please enter a number between 1 and 4.\n" << endl;
+            default: cout << "Invalid Option! Please enter a number between 1 and 5.\n" << endl;
         }
-    } while(choice!=4);
+    } while(choice!=5);
+
+    system("pause");
 
     return 0;
 }
